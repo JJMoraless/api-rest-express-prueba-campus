e97177197900d4e9441aae095d7cc72ba3c5eec1
JJMoraless <jjmorales444@misena.edu.co>
2023-07-16 21:30:58 -0500

    feat: :recycle: cambiado nombre archivos mejor nombre

--- src/controllers/bodegas.controller.js
+++ src/controllers/bodegas.controller.js
@@ -2,14 +2,14 @@ import { bodega } from "../models/bodega.model.js";
 
 export const getBodegas = async (req, res) => {
   const data = await bodega.getBodegas();
-  res.send(data);
+  res.json(data);
 };
 
 export const saveBodega = async ({ body }, res) => {
   try {
     const data = await bodega.saveBodega({ body });
-    res.send(data);
-  } catch (e){
+    res.json(data)
+  } catch (e) {
     res.status(404).json(e.message);
   }
 };
